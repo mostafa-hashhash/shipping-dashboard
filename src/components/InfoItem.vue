c2d74cae4415f46aed6218fb24e487336f9b4ce4
Mostafa Hashhash <mostafa.hashash@robustastudio.com>
2021-11-02 02:30:39 +0200

    retrieving the data and properly display it as the desgin

---
+++ src/components/InfoItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ title }}
+    <span class="font-bold block">{{ value }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "value"],
+};
+</script>
+
+<style></style>
